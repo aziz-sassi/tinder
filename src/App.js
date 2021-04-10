710e740795e240bdb9a54f1c08288012beb3c181
aziz-sassi <sassiaziz50@gmail.com>
2021-04-10 20:05:00 +0100

    making the app header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+   {/* the nav bar */}
+   <Header/>
+   {/* the tinder card */}
+   {/* the swipers */}
+    </div>
+  );
+}
+
+export default App;
